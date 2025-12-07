44e8958495f21e07454e9dbbe43aa190b3395fe4
Haris <muhammadharisyousaf@gmail.com>
2025-12-07 22:35:40 +0500

    Add Prism ORM with local sqlite databse

---
+++ prisma/migrations/20251207163727_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
